6b4052a1f8938ac21db1e12f034c531d966949da
davichodavid <davichodavid@yahoo.com>
2019-06-11 16:36:08 -0400

    added sessions and cookies

--- api/server.js
+++ api/server.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const helmet = require("helmet");
+const session = require("express-session");
+const KnexSessionStore = require("connect-sessions-knex")(session);
 
 const usersRouter = require("../users/users-router");
 const authRouter = require("../auth/auth-router");
 
 const server = express();
-server.use(express.json(), helmet());
+
+const sessionCofig = {
+  name: "money",
+  secret: "What is going on?",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true
+  },
+  store: new KnexSessionStore({
+    knex: require("../data/databaseConfig"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 30
+  })
+};
+
+server.use(express.json(), helmet(), session(sessionCofig));
 
 server.get("/", (req, res) => {
   res.json({ message: "oh we here alright" });
--- auth/restricted.js
+++ auth/restricted.js
@@ -1,20 +1,6 @@
-const bcrypt = require("bcryptjs");
-const Users = require("../users/user-model");
-
 module.exports = function restricted(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "Invalid Creds" });
-        }
-      })
-      .catch(err => res.status(500).json(err));
+  if (req.session && req.session.username) {
+    next();
   } else {
     res.status(400).json({ message: "Please Provide some creds maN" });
   }
